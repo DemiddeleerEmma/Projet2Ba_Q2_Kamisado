2bdaa5c78803dae57ee994c80d829b2dbc653d87
Demiddeleer Emma <emma.demiddeleer@gmail.com>
2026-05-02 15:22:55 +0200

    evaluate -> position bonus

--- test_main.py
+++ test_main.py
@@ -23,7 +23,7 @@ class Test_legal_move:
         }
 
     def test_dark_piece_moves(self):
-        state = self.make_empty_state(current=0)
+        state = self.make_empty_state(0)
         state["board"][3][3][1] = ("red", "dark")
 
         moves = legal_move(state)
@@ -36,7 +36,7 @@ class Test_legal_move:
 
 
     def test_light_piece_moves(self):
-        state = self.make_empty_state(current=1)
+        state = self.make_empty_state(1)
         state["board"][4][4][1] = ("red", "light")
 
         moves = legal_move(state)
@@ -48,7 +48,7 @@ class Test_legal_move:
         assert [[4, 4], [5, 3]] in moves
 
     def test_piece_bloquee(self):
-        state = self.make_empty_state(current=0)
+        state = self.make_empty_state(0)
         state["board"][3][3][1] = ("red", "dark")
         state["board"][2][2][1] = ("red", "light")
         state["board"][2][3][1] = ("blue", "light")
@@ -58,7 +58,7 @@ class Test_legal_move:
         assert [[3, 3], [3, 3]] in moves
 
     def test_obstacle_cut(self):
-        state = self.make_empty_state(current=0)
+        state = self.make_empty_state(0)
         state["board"][5][3][1] = ("red", "dark")
         state["board"][3][3][1] = ("blue", "light")
         moves = legal_move(state)
@@ -67,14 +67,14 @@ class Test_legal_move:
         assert [[5, 3], [2, 3]] not in moves
 
     def test_stay_in_board(self):
-        state = self.make_empty_state(current=0)
+        state = self.make_empty_state(0)
         state["board"][1][0][1] = ("red", "dark") 
         moves = legal_move(state)
         assert [[1, 0], [0, 0]] in moves
         assert [[1, 0], [0, -1]] not in moves
 
     def test_forced_color(self):
-        state = self.make_empty_state(current=0, forced_color="red")
+        state = self.make_empty_state(0,"red")
         state["board"][3][3][1] = ("red", "dark") 
         state["board"][5][5][1] = ("blue", "dark")
         moves = legal_move(state)
@@ -83,7 +83,7 @@ class Test_legal_move:
         assert [5, 5] not in sources
 
     def test_opponent_ignored(self):
-        state = self.make_empty_state(current=0)
+        state = self.make_empty_state(0)
         state["board"][3][3][1] = ("red", "dark") 
         state["board"][5][5][1] = ("blue", "light")
         moves = legal_move(state)
@@ -113,20 +113,20 @@ class Test_make_move:
         }
 
     def test_change_state(self):
-        state = self.make_empty_state(current=0)
+        state = self.make_empty_state(0)
         state["board"][4][4][1] = ("red", "dark")
         make_move(state, [[4, 4], [2, 4]])
         assert state["board"][2][4][1] == ("red", "dark")
         assert state["board"][4][4][1] is None
 
     def test_change_current(self):
-        state = self.make_empty_state(current=0)
+        state = self.make_empty_state(0)
         state["board"][4][4][1] = ("red", "dark")
         make_move(state, [[4, 4], [2, 4]])
         assert state["current"] == 1
 
     def test_change_color(self):
-        state = self.make_empty_state(current=0)
+        state = self.make_empty_state(0)
         state["board"][4][4][1] = ("red", "dark")
         destination_tile_color = state["board"][2][4][0]
         make_move(state, [[4, 4], [2, 4]])
@@ -213,40 +213,86 @@ class Test_evaluate:
     #=================VICTOIRE=================#
 
     def test_light_row_7_light(self):
-        state = self.make_empty_state(current=1)
+        state = self.make_empty_state(1)
         state["board"][7][3][1] = ("red", "light")
         score = evaluate(state)
         assert score == 100000
  
     def test_light_row_7_dark(self):
-        state = self.make_empty_state(current=0)
+        state = self.make_empty_state(0)
         state["board"][7][3][1] = ("red", "light")
         score = evaluate(state)
         assert score == -100000
 
     def test_dark_row_0_dark(self):
-        state = self.make_empty_state(current=0)
+        state = self.make_empty_state(0)
         state["board"][0][3][1] = ("red", "dark")
         score = evaluate(state)
         assert score == 100000
  
     def test_dark_row_0_light(self):
-        state = self.make_empty_state(current=1)
+        state = self.make_empty_state(1)
         state["board"][0][3][1] = ("red", "dark")
         score = evaluate(state)
         assert score == -100000
     
-        #=================Mobility=================#
+    #=================Mobility=================#
     def test_score_mobility(self):
-        state_free = self.make_empty_state(current=0)
-        state_free["board"][4][4][1] = ("red", "dark")
+        state1 = self.make_empty_state(0)
+        state1["board"][4][4][1] = ("red", "dark")
  
-        state_blocked = self.make_empty_state(current=0)
-        state_blocked["board"][4][4][1] = ("red", "dark")
-        state_blocked["board"][3][4][1] = ("blue", "light")
-        state_blocked["board"][3][3][1] = ("pink", "light")
-        state_blocked["board"][3][5][1] = ("green", "light")
+        state2 = self.make_empty_state(0)
+        state2["board"][4][4][1] = ("red", "dark")
+        state2["board"][3][4][1] = ("blue", "light")
+        state2["board"][3][3][1] = ("pink", "light")
+        state2["board"][3][5][1] = ("green", "light")
  
-        assert evaluate(state_free) > evaluate(state_blocked)
+        assert evaluate(state1) > evaluate(state2)
 
+    #=================symetrie=================#
+    def test_opposite_current(self):
+        state0 = self.make_empty_state(0)
+        state0["board"][4][4][1] = ("red", "dark")
+ 
+        state1 = self.make_empty_state(1)
+        state1["board"][4][4][1] = ("red", "dark")
+ 
+        score0 = evaluate(state0)
+        score1 = evaluate(state1)
+        assert score0 == -score1
+
+        #=================Position_bonus=================#
+    def test_dark_advanced(self):
+        state1 = self.make_empty_state(0)
+        state1["board"][1][4][1] = ("red", "dark")
+ 
+        state2 = self.make_empty_state(0)
+        state2["board"][6][4][1] = ("red", "dark")
+ 
+        assert evaluate(state1) > evaluate(state2)
+
+    def test_light_advanced(self):
+        state1 = self.make_empty_state(1)
+        state1["board"][6][4][1] = ("red", "dark")
+
+        state2 = self.make_empty_state(1)
+        state2["board"][1][4][1] = ("red", "dark")
+
+        assert evaluate(state1) > evaluate(state2)
+
+    def test_central_col_dark(self):
+        state_centre = self.make_empty_state(0)
+        state_centre["board"][4][3][1] = ("red", "dark")
+        state_bord = self.make_empty_state(0)
+        state_bord["board"][4][0][1] = ("red", "dark") 
+ 
+        assert evaluate(state_centre) > evaluate(state_bord)
+
+    def test_central_col_light(self):
+        state_centre = self.make_empty_state(1)
+        state_centre["board"][4][3][1] = ("red", "light")
+        state_bord = self.make_empty_state(1)
+        state_bord["board"][4][0][1] = ("red", "light") 
+ 
+        assert evaluate(state_centre) > evaluate(state_bord)
 
