b7b304f2bb07d013bccd68889745dd0ba313b911
Demiddeleer Emma <emma.demiddeleer@gmail.com>
2026-05-05 15:09:34 +0200

    stabilisation serveur

--- main.py
+++ main.py
@@ -3,20 +3,18 @@ import struct
 import json
 import sys
 import time
-
-from serveur import start_serveur, recv_exact
+from serveur import start_serveur
 
 ##===========configuration===========
+
 PORT = 8888
 NAME = "Les Infernales"
 MATRICULES = ["24164","24374"]
 
-        
 ####===========Inscrire l'IA au tournoi##===========
 
 def register(host, port):
     s = socket.socket()
-    s.settimeout(10)
     s.connect((host, port))
 
     message = {
@@ -25,39 +23,39 @@ def register(host, port):
         "name": NAME,
         "matricules": MATRICULES
     }
+
     msg = json.dumps(message).encode()
     s.sendall(struct.pack('I', len(msg)))
     s.sendall(msg)
-
-    raw_length = recv_exact(s, 4)
+    raw_length = s.recv(4)
 
     if len(raw_length) != 4:
         print("Serveur n'a pas répondu")
         return
-
+    
     length = struct.unpack('I', raw_length)[0]
-    raw_response = recv_exact(s, length)
-    response = raw_response.decode()
+    response = s.recv(length).decode()
     s.close()
 
 ##===========LANCER LE JEU===========
+
 def main():
 
     if len(sys.argv) < 3:
         print("Usage: python main.py <host> <port>")
         return
-
+    
     host = sys.argv[1]
     port = int(sys.argv[2])
-
     start_serveur(PORT) 
     register(host, port)
 
     while True:
+
         try:
-            pass
+            time.sleep(1)
+            
         except KeyboardInterrupt:
-            print("Déconnexion")
             break
 
 if __name__ == "__main__":
--- serveur.py
+++ serveur.py
@@ -3,93 +3,88 @@ import threading
 import struct
 import json
 import random
-
 from stratégie import legal_move, negamax_timeout, messages
 
 ##===========Serveur TCP===========
-def recv_exact(conn, n):
-    data = b""
-    while len(data) < n:
-        packet = conn.recv(n - len(data))
-        if not packet:
-            return None
-        data += packet
-    return data
 
 def start_serveur(port):
 
     def handle_client(client, adresse):
-
         print("Connexion reçue")
-        client.settimeout(10)
 
-        try:
+        with client:
 
-           with client:
+            def recv_exact(client, n):
+                data = b""
+                while len(data) < n:
+                    packet = client.recv(n - len(data))
+                    if not packet:
+                        return None
+                    data += packet
+                return data
+            
+            raw_length = recv_exact(client, 4)
 
-                raw_length = recv_exact(client, 4)
-                if raw_length is None:
-                    return
+            if raw_length is None:
+                return
+            
+            length = struct.unpack('I', raw_length)[0]
+            data = recv_exact(client, length)
 
-                length = struct.unpack('I', raw_length)[0]
+            if data is None:
+                return
+            
+            data = data.decode().strip()
+            req = json.loads(data)
 
-                data = recv_exact(client, length)
-                if data is None:
-                    return
+            if req["request"] == "ping":
 
-                data = data.decode().strip()
-                req = json.loads(data)
-
-            #############################
-
-                if req["request"] == "ping":
-
+                try:
                     res = {"response": "pong"}
                     msg = json.dumps(res).encode()
-                    client.sendall(struct.pack('I', len(msg)))
-                    client.sendall(msg)
+                    client.sendall(struct.pack('I', len(msg)) + msg )
                     print("pong envoyé")
 
+                except Exception as e:
+                    print("Connexion fermée après ping:", e)
+                    return
+                
+            elif req["request"] == "play":
 
-                elif req["request"] == "play":
+                try:
                     print("demande de jeu")
                     state = req["state"]
                     moves = legal_move(state)
 
                     if not moves:
                         move = [[0, 0], [0, 0]]
+
                     else:
                         _, move = negamax_timeout(state, 25 , 2.9 )
+
                     message = random.choice(messages)
-
                     print(move)
-
                     res = {
                         "response": "move",
                         "move": move,
                         "message": message
                     }
-
                     msg = json.dumps(res).encode()
-                    client.sendall(struct.pack('I', len(msg)))
-                    client.sendall(msg)
-                  
-        except Exception as e:
-                
-            print("Connexion fermée après ping:", e)
-        except Exception as e:
-                print("Erreur play:", e)
+                    client.sendall(struct.pack('I', len(msg)) + msg )
 
-    
+                except Exception as e:
+                    print("Erreur play:", e)
+                    return
+
     def loop():
+
         s = socket.socket()
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind(("0.0.0.0", port))
         s.listen()
-        
+
         while True:
             client, adresse = s.accept()
             threading.Thread(target=handle_client, args=(client, adresse), daemon=True).start()
             #handle_client(client, adresse)
-
     threading.Thread(target=loop, daemon=True).start()
